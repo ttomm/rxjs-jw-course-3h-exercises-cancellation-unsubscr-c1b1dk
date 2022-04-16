c837f65c67adbedc87ff89d119db8096a170e2aa
ttomm <to_scan@autograf.pl>
2022-04-16 16:46:36 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,2 +1,15 @@
+import { Observable } from 'rxjs';
 
+const observable$ = new Observable<string>((subscriber) => {
+  let counter = 1;
+  setInterval(() => {
+    subscriber.next('Times ' + counter);
+    ++counter;
+  }, 1000);
 
+  return () => console.log('TeardownLogic');
+});
+
+const sub = observable$.subscribe((txt) => console.log(txt));
+
+setTimeout(() => sub.unsubscribe(), 7000);
